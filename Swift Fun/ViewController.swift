bfe8646e1ba75c93d182ef2faf1fa4ee855e380d
Rico Alderete <su8lim3@gmail.com>
2019-04-18 20:42:55 -0600

    Playing around with calculations and text fields

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,33 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var buttonCount = 0
+//    var buttonCount = 0
     
     @IBOutlet weak var myLabel: UILabel!
     
     
+    @IBOutlet weak var topTextField: UITextField!
+    
+    
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    
     @IBAction func buttonTapped(_ sender: Any) {
-        buttonCount = buttonCount + 1
         
-        print(buttonCount)
+        let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+        myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
         
-        if buttonCount >= 10 {
-            view.backgroundColor = UIColor.red
-            let labelText = "You hit it more than 10 times"
-            myLabel.text = labelText
-        }
         
-        if buttonCount >= 15 {
-            view.backgroundColor = UIColor.green
-            let labelText = "You hit it more than 15 times"
-            myLabel.text = labelText
-        }
-        
-        if buttonCount >= 20 {
-            view.backgroundColor = UIColor.blue
-            let labelText = "Loook Madison, I hit it 20 times!"
-            myLabel.text = labelText
-        }
+//        buttonCount += 1
+//
+//        print(buttonCount)
+//
+//        if buttonCount >= 10 {
+//            view.backgroundColor = UIColor.red
+//            let labelText = "You hit it more than 10 times"
+//            myLabel.text = labelText
+//        }
+//
+//        if buttonCount >= 15 {
+//            view.backgroundColor = UIColor.green
+//            let labelText = "You hit it more than 15 times"
+//            myLabel.text = labelText
+//        }
+//
+//        if buttonCount >= 20 {
+//            view.backgroundColor = UIColor.blue
+//            let labelText = "Loook Madison, I hit it 20 times!"
+//            myLabel.text = labelText
+//        }
+//
         
     }
     
