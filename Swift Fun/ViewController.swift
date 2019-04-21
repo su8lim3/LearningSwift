e8236867bafdba28314a6eb341a0aadf781e7d8b
Rico Alderete <su8lim3@gmail.com>
2019-04-21 11:33:35 -0600

    More Swift Fun training

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -20,11 +20,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var bottomTextField: UITextField!
     
+    @IBOutlet weak var additionSwitch: UISwitch!
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
-        myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        let addition = additionSwitch.isOn
+        
+        if addition{
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+            
+        } else {
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+        }
+        
+        
         
         
 //        buttonCount += 1
